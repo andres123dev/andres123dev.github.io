ae75c52dd431af75ac37fdc29280f311ffb54c5e
Andrés Fernández Burón <193231832+andres123dev@users.noreply.github.com>
2025-02-07 23:56:42 +0100

    Styles updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /** Andrés Fernández Burón
- *  Noviembre de 2021
+ *  Noviembre de 2020
  *
  *
  *  Aplicación web: Mi sitio web
@@ -8,11 +8,16 @@
  *
  */
 "use strict";
+function isAppleDevice() {
+    if (/^(iPhone|iPod|iPad|Mac OS)/.test(navigator.userAgent)) {
+        return true;
+    }
+    return false;
+}
 var mainMenuPanel;
 var menuSwitchButton;
 var closerElements;
 function addListenerBtnMenu() {
-    closerElements = document.querySelectorAll('figure, main, footer');
     mainMenuPanel = document.querySelector('nav');
     if (mainMenuPanel)
         mainMenuPanel.style.display = 'none';
@@ -20,6 +25,7 @@ function addListenerBtnMenu() {
     if (mainMenuPanel && menuSwitchButton) {
         menuSwitchButton.addEventListener('click', function () {
             mainMenuPanel.style.display = (mainMenuPanel.style.display == 'none') ? 'block' : 'none';
+            closerElements = document.querySelectorAll('figure, main, footer');
             for (let i = 0; i < closerElements.length; i++) {
                 closerElements[i].addEventListener('click', function () {
                     mainMenuPanel.style.display = 'none';
@@ -28,18 +34,22 @@ function addListenerBtnMenu() {
         });
     }
 }
+var menuSubPanelSwitchButtons;
+var submenuPanels;
 function addListenerBtnSubmenu() {
-    const menuSupanelSwitchButtons = document.querySelectorAll('.boton-mostrar-submenu');
-    if (menuSupanelSwitchButtons) {
-        for (let i = 0; i < menuSupanelSwitchButtons.length; i++) {
-            var submenuPanel = menuSupanelSwitchButtons[i].nextElementSibling;
-            if (submenuPanel)
-                submenuPanel.style.display = 'none';
-            var btn = menuSupanelSwitchButtons[i];
-            if (btn) {
-                btn.addEventListener('click', function () {
-                    submenuPanel.style.display = (submenuPanel.style.display == 'none') ? 'block' : 'none';
-                    btn.style.backgroundColor = (btn.style.backgroundColor == 'whitesmoke') ? 'lightseagreen' : 'whitesmoke';
+    menuSubPanelSwitchButtons = document.querySelectorAll('.boton-mostrar-submenu');
+    submenuPanels = Array();
+    if (menuSubPanelSwitchButtons) {
+        for (let i = 0; i < menuSubPanelSwitchButtons.length; i++) {
+            submenuPanels[i] = menuSubPanelSwitchButtons[i].nextElementSibling;
+            if (menuSubPanelSwitchButtons[i].nextElementSibling) {
+                submenuPanels[i].style.display = 'none';
+            }
+            if (menuSubPanelSwitchButtons[i]) {
+                menuSubPanelSwitchButtons[i].addEventListener('click', function (evt) {
+                    if (submenuPanels[i]) {
+                        submenuPanels[i].style.display = (submenuPanels[i].style.display == 'none') ? 'block' : 'none';
+                    }
                 });
             }
         }
@@ -48,4 +58,25 @@ function addListenerBtnSubmenu() {
 document.addEventListener('DOMContentLoaded', function () {
     addListenerBtnMenu();
     addListenerBtnSubmenu();
+    if (isAppleDevice()) {
+        let message = `Aún hace falta ajustar el diseño responsivo del sitio 
+        para que se vea correctamente en los dispositivos de Apple.`;
+        let paragraph = document.createElement('p');
+        paragraph.innerText = message;
+        let container = document.querySelector('main > section > article:nth-of-type(2)');
+        if (container) {
+            container.appendChild(paragraph);
+        }
+    }
+    document.oncopy = function () {
+        alert('TeCodeo.com\nCopyright 2020-2021\nTodos los derechos reservados');
+        return false;
+    };
+    document.oncut = function () {
+        alert('TeCodeo.com\nCopyright 2020-2021\nTodos los derechos reservados');
+        return false;
+    };
+    window.onload = function () { document.onselectstart = function () { return false; }; };
+    document.oncontextmenu = function () { return false; };
+    window.onload = function () { document.onmousedown = function () { return false; }; };
 });
